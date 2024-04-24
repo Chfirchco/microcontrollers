9e88b4a37aa72a67cfa87903bd92f7016ae3d650
Vloodek <147373107+Vloodek@users.noreply.github.com>
2024-04-24 14:51:45 +0800

    Create led_cube.ino

---
+++ LedCube/led_cube.ino
@@ -0,0 +1,19 @@
+
+int ledPins[8] = {3, 5, 6, 9, 12, 13}; // Определение 8 пинов для светодиодов
+
+int delayTime = 100; // Задержка между переключениями
+
+void setup() {
+  for (int i = 0; i < 8; i++) { // Настройка пинов как OUTPUT
+    pinMode(ledPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 256; i++) { // Цикл по 256 комбинациям
+    for (int j = 0; j < 8; j++) { // Цикл по 8 светодиодам
+      digitalWrite(ledPins[j], (bitRead(i, j) == 1) ? HIGH : LOW); // Включение/выключение светодиода
+    }
+    delay(delayTime); // Задержка между комбинациями
+  }
+}
