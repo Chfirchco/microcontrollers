ae88eda1fdc68e9d688a633b1e1dfc2e405f1a85
Vloodek <vlad.nikz45@gmail.com>
2024-06-01 10:34:19 +0800

    да

---
+++ project/firmware/mqtt_connect.h
@@ -0,0 +1,35 @@
+#ifndef MQTT_CONNECT_H
+#define MQTT_CONNECT_H
+
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+
+// MQTT server details
+const char* mqtt_server = "public.mqtthq.com";
+const int mqtt_port = 1883;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void MQTTcallback(char* topic, byte* payload, unsigned int length);
+
+void setupMQTT(String topics[]) {
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(MQTTcallback);
+  
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP8266")) {
+      Serial.println("connected");
+      for (int i = 0; i < 4; ++i) {
+        client.subscribe(topics[i].c_str());
+        Serial.println(topics[i]);
+      }
+    } else {
+      Serial.print("failed with state ");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
+}
+
+#endif
